774763ea5ba771268305d397c9bcf4cbe612437c
Vicente Monsalve <vicente.monsalve@gmail.com>
2023-07-21 17:54:53 -0400

    creando widgets reutilizables, en este caso el widget styled_text

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hm/styled_text.dart';
 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class GradiantContainer extends StatelessWidget {
   const GradiantContainer({super.key});
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-            colors: [Colors.black, Colors.grey, Colors.white],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight),
+            colors: const [Colors.black, Colors.grey, Colors.white],
+            begin: startAlignment,
+            end: endAlignment),
       ),
-      child: const Center(child: StyledText()),
+      child: const Center(child: StyledText('Hola Vicente')),
     );
   }
 }
--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 class StyledText extends StatelessWidget {
-  const StyledText({super.key});
+  const StyledText(this.text, {super.key});
+  final String text;
   @override
   Widget build(context) {
-    return const Text(
-      'Hola Mundo!',
-      style: TextStyle(
+    return Text(
+      text,
+      style: const TextStyle(
         color: Colors.white,
         fontSize: 28,
       ),
